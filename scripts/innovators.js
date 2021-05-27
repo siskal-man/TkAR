1d7f21be8b935cce378296388b025ed2d13238c9
Codirovshik <block-code@mail.ru>
2021-05-27 17:50:39 +0300

    нужно теперь записать обращения и почистить проект

--- scripts/innovators.js
+++ scripts/innovators.js
@@ -3,9 +3,6 @@ let pauseBtn = document.querySelector('.pause');
 let backBtn = document.querySelector('.back-button__link');
 let tigerSound = document.querySelector('.tigerSound');
 
-backBtn.addEventListener('click', function(){
-    open('https://10.1.4.233:5500/index.html');
-})
 
 playBtn.addEventListener('click', function(){
     tigerSound.play();
